8eaa8e808838a3ed3c19cd7bcca824df006823da
Graham Lynch <graham.lynch@gmail.com>
2024-07-04 02:30:42 +0700

    Updated docusaurus config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'glynch', // Usually your GitHub org/user name.
   projectName: 'jollama', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
